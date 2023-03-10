f3e2429b5a297d4e5974f832e6bc0caeaa9b79ea
JiyaTeresa <jiyateresa@gmial.com>
2023-03-10 15:02:21 +0530

    arithmetic operations

---
+++ pgm3.sh
@@ -0,0 +1,17 @@
+echo "enter two numbers"
+read a
+read b
+s=`expr $a + $b`
+echo "sum is $s"
+m=`expr $a - $b`
+echo "diffrence is $m"
+p=`expr $a \* $b`
+echo "product is $p"
+d=`expr $a / $b`
+echo "division is $d"
+
+
+
+
+
+
