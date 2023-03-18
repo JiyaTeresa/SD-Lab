52743830692a2b63e09a17a9d4d5b91d79cdcac1
JiyaTeresa <jiyateresa@gmail.com>
2023-03-18 15:19:32 +0530

    prime or not

--- pgm6.sh
+++ pgm6.sh
@@ -1,19 +1,18 @@
-echo "enter three numbers"
+echo "enter 3 numbers"
 read a
 read b
 read c
-if [ $a  -gt $b ]
+if [ $a -gt $b ]
 then
-if [$a -gt $c ]
+if [ $a -gt $c ]
 then
 echo "largest is $a"
 else
 echo "largest is $c"
-fi
+fi 
 elif [ $b -gt $c ]
 then
 echo "largest is $b"
 else
 echo "largest is $c"
 fi
-
